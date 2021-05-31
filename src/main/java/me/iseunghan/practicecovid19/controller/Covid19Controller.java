f323711ee52533a388187dd5359df1f09b0469a8
iseunghan <tmdgks301@naver.com>
2021-05-31 16:15:51 +0900

    Covid19Controller 시,도 감염현황 조회
    - Covid19Controller.get_covid19_sido_info : 전국 시,도 코로나19 감염현황 조회
    - Covid19Controller.getCovidRequestURI : 요청 URI 만들어주는 메소드 생성

--- src/main/java/me/iseunghan/practicecovid19/controller/Covid19Controller.java
+++ src/main/java/me/iseunghan/practicecovid19/controller/Covid19Controller.java
@@ -23,13 +23,18 @@ import java.util.List;
  * 21.05.24 월 : 코로나 api 호출 후 응답 결과까지 얻기 완료.
  * 21.05.25 화 : XmlMapper 의존성 추가 후 데이터 매핑 완료.
  */
-@Controller
+@RestController
 @RequestMapping(value = "/covid19-api")
 public class Covid19Controller {
 
-    @GetMapping
-    @ResponseBody
-    public String covid_api_Test() throws UnsupportedEncodingException, URISyntaxException {
+    static String covid19Inf = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson";
+    static String covid19SidoInf = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+
+    /**
+     * 전국 시,도의 코로나19 감염현황을 제공해줍니다.
+     */
+    @GetMapping("/local")
+    public ResponseEntity get_covid19_sido_info() throws UnsupportedEncodingException {
         RestTemplate rt = new RestTemplate();
         // RestTemplate의 MessageConverter가 기본 Charset이 UTF-8이 아니여서 설정해줘야 함!
         rt.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
@@ -38,23 +43,40 @@ public class Covid19Controller {
         MediaType mediaType = MediaType.valueOf("application/xml;charset=UTF-8");
         headers.setContentType(mediaType);
 
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson");
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=8dg1oO8E7c1WbwllHQfIkNvUulM0K%2Fbg7dzt25DwEgO3R9Cv6w10zlsQ269Tq5pQA3XtPziQLkXCzW4ftvTGzw%3D%3D"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode("20200410", "UTF-8")); /*검색할 생성일 범위의 시작*/
-        urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode("20200410", "UTF-8")); /*검색할 생성일 범위의 종료*/
-        URI uri = new URI(urlBuilder.toString());
+        URI covidRequestURI = getCovidRequestURI(covid19SidoInf, 1);
 
         HttpEntity http = new HttpEntity<>(headers);
 
         ResponseEntity<String> response = rt.exchange(
-                uri,
+                covidRequestURI,
                 HttpMethod.GET,
                 http,
                 String.class
         );
 
         return "응답 결과 : " + response.getBody();
+    }
+
+    /**
+     * 조회 시작일, 종료일에 대해서 요청 URI 생성해주는 메소드
+     *
+     * @param days 현재로부터 {days}일전까지 조회
+     * @return
+     */
+    private URI getCovidRequestURI(String url, int days) throws UnsupportedEncodingException {
+        // days
+        LocalDate now = LocalDate.now();
+        LocalDate yst = LocalDate.of(now.getYear(), now.getMonth(), now.getDayOfMonth() - days);
+        String startCreateDt = yst.toString().replaceAll("-", "");
+        String endCreateDt = now.toString().replaceAll("-", "");
+
+        StringBuilder urlBuilder = new StringBuilder(url);
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=8dg1oO8E7c1WbwllHQfIkNvUulM0K%2Fbg7dzt25DwEgO3R9Cv6w10zlsQ269Tq5pQA3XtPziQLkXCzW4ftvTGzw%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("startCreateDt", "UTF-8") + "=" + URLEncoder.encode(startCreateDt, "UTF-8")); /*검색할 생성일 범위의 시작*/
+        urlBuilder.append("&" + URLEncoder.encode("endCreateDt", "UTF-8") + "=" + URLEncoder.encode(endCreateDt, "UTF-8")); /*검색할 생성일 범위의 종료*/
+        URI uri = URI.create(urlBuilder.toString());
+        return uri;
     }
 }
